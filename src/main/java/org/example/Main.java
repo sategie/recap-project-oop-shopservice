7e0d294011a52a750bfec5795dfd7edfe652ca7a
sategie <sategie@yahoo.com>
2024-08-29 16:40:29 +0200

    Create methods to get a single product and get all products.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ProductRepo stock1 = new ProductRepo();
+//        ProductRepo stock2 = new ProductRepo();
+//        ProductRepo stock3 = new ProductRepo();
+
+        System.out.println(stock1);
+        stock1.addProduct(new Product("Laptop", 200, 5, 20));
+//        stock2.addProduct(new Product("Laptop", 200, 5, 20));
+//        stock3.addProduct(new Product("Laptop", 200, 5, 20));
+        System.out.println(stock1);
+        System.out.println(stock1.getAllProducts());
     }
 }
--- src/main/java/org/example/ProductRepo.java
+++ src/main/java/org/example/ProductRepo.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepo {
-//    Implement ProductRepo class with a list to store products
+//    Implement ProductRepo class with a list to store products (from the Product record)
     private final List<Product> products = new ArrayList<Product>();
 
 //    addProduct method
@@ -15,5 +15,24 @@ public class ProductRepo {
 //    removeProduct method
     public void removeProduct(Product product){
         products.remove(product);
+    }
+
+//    Method to get single product
+    public Product getSingleProduct(int index){
+        return products.get(index);
+    }
+
+//    Method to get all products
+    public List<Product> getAllProducts(){
+//        Create a copy of the product list to prevent modification of the initial Arraylist
+        return new ArrayList<>(products);
+    }
+
+//    toString() method to display output in human-readable form
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "products=" + products +
+                '}';
     }
 }
